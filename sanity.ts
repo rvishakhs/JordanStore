24a5b446d728bd16254f78967e674cc589e4a78d
rvishakhs <rvishakhs@gmail.com>
2022-11-20 11:03:21 +0000

    Final commit

--- sanity.ts
+++ sanity.ts
@@ -4,6 +4,7 @@ import createImageUrlBuilder from "@sanity/image-url"
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    token : process.env.SANITY_API_TOKEN,
     projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     apiVersion: "2021-03-25",
     useCdn: process.env.NODE_ENV === "production",
